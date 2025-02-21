962af320a56898f99853b60c7c25ea849e1c694a
Mathias Sørensen <mkrs24@student.aau.dk>
2025-02-21 22:08:53 +0100

    Made it possible to switch on/off hidden files in neotree with the shortcut <leader> h

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -10,16 +10,44 @@ return {
     vim.keymap.set("n", "<C-n>", ":Neotree toggle <CR>", {})
     vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 
-    require("neo-tree").setup({
+    local neo_tree = require("neo-tree")
+    local hidden_visible = false
+
+    -- Function to toggle hidden files
+    local function toggle_hidden_files()
+      hidden_visible = not hidden_visible
+
+      -- Close Neo-tree before updating settings
+      vim.cmd("Neotree close")
+
+      -- Apply new settings
+      neo_tree.setup({
+        filesystem = {
+          filtered_items = {
+            visible = hidden_visible,
+            hide_dotfiles = not hidden_visible,
+            hide_gitignored = not hidden_visible,
+            hide_hidden = not hidden_visible,
+          },
+        },
+      })
+
+      -- Reopen Neo-tree to apply new settings
+      vim.cmd("Neotree show")
+    end
+
+    -- Set keybinding to toggle hidden files
+    vim.keymap.set("n", "<leader>h", toggle_hidden_files, { desc = "Toggle Hidden Files in Neo-tree" })
+
+    -- Default setup
+    neo_tree.setup({
       filesystem = {
-        filtered = {
-          visible = true,
-          hide_dotfiles = false,
-          hide_gitignore = false,
-          hide_hidden = false,
-          hide_by_name = {},
-          hide_by_pattern = {},
-        }
+        filtered_items = {
+          visible = hidden_visible,
+          hide_dotfiles = not hidden_visible,
+          hide_gitignored = not hidden_visible,
+          hide_hidden = not hidden_visible,
+        },
       },
       event_handlers = {
         {
@@ -32,4 +60,4 @@ return {
     })
   end,
 }
-         
+

09c119bab6d09f8d745cc0e425c96c0ba1a4b814
Mathias Sørensen <mkrs24@student.aau.dk>
2025-03-06 10:56:05 +0100

    Added costum logo and a hidden file-live-grep

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -17,6 +17,9 @@ return {
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>fh", function()
+  require("telescope.builtin").live_grep({ additional_args = { "--hidden" } })
+end, { noremap = true, silent = true })
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
 
       vim.keymap.set("n", "<leader>fw", function() builtin.grep_string({ search = vim.fn.expand("<cword>") }) end, { noremap = true, silent = true })

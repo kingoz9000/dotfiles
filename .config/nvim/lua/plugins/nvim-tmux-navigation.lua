7f7a8aad58a9a6ad89220dac9ed41f7200bc1d2f
Mathias Sørensen <mkrs24@student.aau.dk>
2025-03-14 23:51:32 +0100

    Added convenient command to run python script easy and in vim with <leader>r

--- .config/nvim/lua/plugins/nvim-tmux-navigation.lua
+++ .config/nvim/lua/plugins/nvim-tmux-navigation.lua
@@ -6,5 +6,7 @@ return {
     vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", {})
     vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", {})
     vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", {})
+    vim.api.nvim_set_keymap('n', '<leader>r', ':w<CR>:silent !tmux split-window -v "source $VIRTUAL_ENV/bin/activate; python3 %; echo \\"Press Enter to close...\\"; read -r dummy"<CR>', { noremap = true, silent = true })
+
   end,
 }

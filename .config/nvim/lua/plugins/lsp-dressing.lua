4a57be50653091233ffc1c2a7711502da55b73e5
Mathias Sørensen <mkrs24@student.aau.dk>
2025-04-08 19:12:59 +0200

    Added replacing for characters and words in text files

--- .config/nvim/lua/plugins/lsp-dressing.lua
+++ .config/nvim/lua/plugins/lsp-dressing.lua
@@ -12,6 +12,42 @@ return {
         },
       },
     })
-  end
+
+    -- 🔁 Word-level substitution: :PromptSub or <leader>r
+    vim.api.nvim_create_user_command("PromptSub", function()
+      vim.ui.input({ prompt = "Word to replace: " }, function(from)
+        if not from or from == "" then return end
+        vim.ui.input({ prompt = "Replace with: " }, function(to)
+          if not to then return end
+          vim.cmd(string.format("%%s/\\<%s\\>/%s/g", from, to))
+        end)
+      end)
+    end, {})
+
+    vim.keymap.set("n", "<leader>w", function()
+      vim.cmd("PromptSub")
+    end, { desc = "Prompted word substitution", noremap = true, silent = true })
+
+    -- 🔤 Character-level substitution: :PromptCharSub or <leader>c
+    vim.api.nvim_create_user_command("PromptCharSub", function()
+      vim.ui.input({ prompt = "Character to replace: " }, function(from)
+        if not from or #from ~= 1 then
+          vim.notify("Please enter a single character", vim.log.levels.WARN)
+          return
+        end
+        vim.ui.input({ prompt = "Replace with: " }, function(to)
+          if not to or #to ~= 1 then
+            vim.notify("Please enter a single replacement character", vim.log.levels.WARN)
+            return
+          end
+          vim.cmd(string.format("%%s/%s/%s/g", vim.fn.escape(from, "/\\"), vim.fn.escape(to, "/\\")))
+        end)
+      end)
+    end, {})
+
+    vim.keymap.set("n", "<leader>c", function()
+      vim.cmd("PromptCharSub")
+    end, { desc = "Prompted character substitution", noremap = true, silent = true })
+  end,
 }
 
